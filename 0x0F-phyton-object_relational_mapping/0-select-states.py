3e8ed086aff134a6a0d375b83b5b9e22d5061eaa
paulgi48 <paulgiovannio@outlook.com>
2023-11-23 21:03:46 +0100

    alx push

--- 0x0F-phyton-object_relational_mapping/0-select-states.py
+++ 0x0F-phyton-object_relational_mapping/0-select-states.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_Oe_O_usa """ 
+import MySQLdb 
+import sys
 
+if __лаmе__ == "__main__":
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+cur = db.cursorO
+cur.execute("SELECT * FROM states")
+rows = cur. fetchaall()
+ for row in rows:
+    print(row)
+cur.close
+db.close
--- 0x0F-phyton-object_relational_mapping/0-select-states.sql
+++ 0x0F-phyton-object_relational_mapping/0-select-states.sql
@@ -1,7 +1,8 @@
-CREATE DATABASE IF NOT EXISTS hbtn_0_0_usa;
+-- Create states table in hbtn_Oe_O_usa with some data 
+CREATE DATABASE IF NOT EXISTS hbtn_Oe_O_usa;
 USE hbtn_Oe_O_usa;
 CREATE TABLE IF NOT EXISTS states (
-id INT NOT NULL AUTO_INCREMENT, name VARCHAR (256) NOT NULL
-PRIMARY KEY id)
-D;
-INSERT INTO states (name) VALUES ("California"'), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+id INT NOT NULL AUTO_INCREMENT, 
+name VARCHAR (256) NOT NULL,
+PRIMARY KEY (id));
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
