b26f4ca8be98ee86f91ab81e1adf8db37abe6b08
paulgi48 <paulgiovannio@outlook.com>
2023-11-23 18:00:54 +0100

    alx

---
+++ 0x0F-phyton-object_relational_mapping/0-select-states.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS hbtn_0_0_usa;
+USE hbtn_Oe_O_usa;
+CREATE TABLE IF NOT EXISTS states (
+id INT NOT NULL AUTO_INCREMENT, name VARCHAR (256) NOT NULL
+PRIMARY KEY id)
+D;
+INSERT INTO states (name) VALUES ("California"'), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
