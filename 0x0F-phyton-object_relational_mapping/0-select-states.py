1bcd254fd3699d5897fdc9901b3324be58cf1d01
paulgi48 <paulgiovannio@outlook.com>
2023-11-24 09:47:58 +0100

    orm 2

--- 0x0F-phyton-object_relational_mapping/0-select-states.py
+++ 0x0F-phyton-object_relational_mapping/0-select-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_Oe_O_usa """ 
+""" lists all states from the database hbtn_0e_0_usa """ 
 import MySQLdb 
 import sys
 
@@ -7,7 +7,7 @@ if __лаmе__ == "__main__":
         db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 cur = db.cursorO
 cur.execute("SELECT * FROM states")
-rows = cur. fetchaall()
+rows = cur. fetchall()
  for row in rows:
     print(row)
 cur.close
